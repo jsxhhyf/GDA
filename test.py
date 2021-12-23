fe118897aaa5701ce50c6795046c5e1b6e048426
Phillip <jsxhhyf@gmail.com>
2021-12-23 20:02:01 +1100

    Improvement 1. [ADD] ability to deal with snapshot and delta data

--- test.py
+++ test.py
@@ -9,19 +9,28 @@ class WebsocketHandler:
     Connector for Bybit's WebSocket API.
     """
 
-    def __init__(self, endpoint, subs):
+    def __init__(self, endpoint, subscriptions):
         self.ws = WebSocket(
             endpoint,
-            subscriptions=subs,
+            subscriptions=subscriptions,
             trim_data=False
         )
         self.lob = LOB()
 
-    def fetch(self, topic):
-        data_json = self.ws.fetch(topic)
-        if data_json.get('type') == 'snapshot':
-            self.lob.fit_snapshot(data_json)
-        elif data_json.get('type') == 'delta':
+    def fetch(self, topic, n=3):
+        while n > 0:
+            data_json = self.ws.fetch(topic)
+            if data_json:
+                if data_json.get('type') == 'snapshot':
+                    n -= 1
+                    logger.debug('got snapshot')
+                    self.lob.fit_snapshot(data_json)
+                elif data_json.get('type') == 'delta':
+                    n -= 1
+                    self.lob.fit_delta(data_json)
+                    logger.debug('got delta')
+
+        self.lob.save()
 
 
 class LOB:
@@ -30,7 +39,7 @@ class LOB:
     """
 
     def __init__(self):
-        self.lob_detail = None
+        self.lob_detail = np.empty([1, 18])
         self.lob_general = {}
         self.trade = None
         self.event_n = 0
@@ -39,21 +48,55 @@ class LOB:
         data = j.get('data')
         seq = j.get('cross_seq')
         ts = j.get('timestamp_e6')
-
         # [order_id, side, price, size, lob_action, event_ts, sx_ts, rx_ts, order_type, order_executed, execution_price,
         # executed_size, agressor_side, matching_order_id, old_order_id, trade_id, size_ahead, orders_ahead]
 
-        temp = [data.get('id'), Util.d_buy_sell[data.get('side')], data.get('price'), data.get('size'), np.nan, ts,
-                np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
-        self.lob_detail = np.array(temp)
+        for record in data:
+            temp = [record.get('id'), Util.d_buy_sell[record.get('side')], record.get('price'), record.get('size'),
+                    np.nan, ts, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
+            self.lob_detail = np.append(self.lob_detail, np.array(temp, ndmin=2), axis=0)
 
     def fit_delta(self, j: "json"):
         data = j.get('data')
         seq = j.get('cross_seq')
         ts = j.get('timestamp_e6')
 
-        # [order_id, side, price, size, lob_action, event_ts, sx_ts, rx_ts, order_type, order_executed, execution_price,
-        # executed_size, agressor_side, matching_order_id, old_order_id, trade_id, size_ahead, orders_ahead]
+        d = data.get('delete')
+        if d:
+            for record in d:
+                _id = record.get('id')
+                idx_to_delete = np.where(self.lob_detail[:, 0] == _id)
+                if len(idx_to_delete) != 0:
+                    self.lob_detail = np.delete(self.lob_detail, idx_to_delete, axis=0)
+                else:
+                    logger.debug("no record to delete")
+
+        u = data.get('update')
+        if u:
+            for record in u:
+                _id = record.get('id')
+                idx_to_udpate = np.where(self.lob_detail[:, 0] == _id)
+                if len(idx_to_udpate) != 0:
+                    self.lob_detail[idx_to_udpate, 3] = record.get('size')
+                else:
+                    logger.debug("no record to update")
+
+        i = data.get('insert')
+        if i:
+            for record in u:
+                _id = record.get('id')
+                idx_to_insert = np.where(self.lob_detail[:, 0] == _id)
+                if len(idx_to_insert) == 0:
+                    temp = [record.get('id'), Util.d_buy_sell[record.get('side')], record.get('price'),
+                            record.get('size'), np.nan, ts, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan,
+                            np.nan, np.nan, np.nan, np.nan, np.nan]
+                    np.append(self.lob_detail, temp, axis=0)
+                else:
+                    logger.debug("record exists")
+
+    def save(self):
+        np.savetxt('./lob_detail.csv', self.lob_detail, delimiter=',', fmt='%s')
+
 
 class Util:
     d_buy_sell = {'Buy': 1, 'Sell': 2, 'Unkonwn': 0}
@@ -64,12 +107,10 @@ class Util:
 
 
 if __name__ == '__main__':
-
     url = "wss://stream-testnet.bybit.com/realtime"
     subs = [
         "orderBook_200.100ms.BTCUSD"
     ]
 
-    handler = WebsocketHandler(endpoint=url, subs=subs)
-    while True:
-        handler.fetch()
+    handler = WebsocketHandler(endpoint=url, subscriptions=subs)
+    handler.fetch(subs[0])
